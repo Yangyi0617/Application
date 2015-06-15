866d19698bfe04372f93ed6037244b8ac93b1fd7
Yangyi0617 <yangyi0617@foxmail.com>
2015-06-15 16:57:49 +0430

    Create ReadIRquestHandler.java

---
+++ ReadIRquestHandler.java
@@ -0,0 +1,24 @@
+package com.zhs.server;
+
+import edu.kit.aifb.dbe.hermes.IRequestHandler;
+import edu.kit.aifb.dbe.hermes.Request;
+import edu.kit.aifb.dbe.hermes.Response;
+
+public class ReadIRequestHandler implements IRequestHandler {
+
+	public Response handleRequest(Request arg0) {
+		return null;
+
+	}
+
+	public boolean hasPriority() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean requiresResponse() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
